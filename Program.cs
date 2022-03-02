9ea2c103f2f4094fcc0564684467a558179d188f
M Sahithi <msahithi254@gmail.com>
2022-03-02 14:02:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace @params
+{
+    internal class Program
+    {
+        public static int Add(params int[] numbers)
+        {
+            int total = 0;
+            foreach(int i in numbers)
+            {
+                total += i;
+            }
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            int y = Add(1, 2, 3, 4, 5);
+            Console.WriteLine(y);
+            Console.ReadLine();
+
+        }
+    }
+}
+
